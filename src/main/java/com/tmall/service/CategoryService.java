22bc588eff4eef4df671ec2fb30b49686b205786
GRedComeT <a18985113153@gmail.com>
2021-04-02 15:22:33 +0800

    Finishing "search catagory"

---
+++ src/main/java/com/tmall/service/CategoryService.java
@@ -0,0 +1,20 @@
+package com.tmall.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.tmall.dao.CategoryDAO;
+import com.tmall.pojo.Category;
+
+@Service
+public class CategoryService {
+    @Autowired CategoryDAO categoryDAO;
+
+    public List<Category> list() {
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        return categoryDAO.findAll(sort);
+    }
+}
