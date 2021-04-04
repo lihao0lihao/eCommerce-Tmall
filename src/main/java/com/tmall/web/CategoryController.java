a0abf04d4e86ab2d7fdf95020e8b571a99e8d605
GRedComeT <a18985113153@gmail.com>
2021-04-04 18:03:07 +0800

    add paging function in back-end

--- src/main/java/com/tmall/web/CategoryController.java
+++ src/main/java/com/tmall/web/CategoryController.java
@@ -2,8 +2,10 @@ package com.tmall.web;
 
 import com.tmall.pojo.Category;
 import com.tmall.service.CategoryService;
+import com.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,8 +15,10 @@ public class CategoryController {
     @Autowired CategoryService categoryService;
 
     @GetMapping("/categories")
-    public List<Category> list() throws Exception {
-        return categoryService.list();
+    public Page4Navigator<Category> list(@RequestParam(value = "start", defaultValue = "0") int start, @RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
+        start = start<0?0:start;
+        Page4Navigator<Category> page =categoryService.list(start, size, 5);  //5表示导航分页最多有5个，像 [1,2,3,4,5] 这样
+        return page;
     }
 
 }
