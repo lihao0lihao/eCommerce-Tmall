ddda4a081cf0c18727a857472f5ad8eafa95d8ff
GRedComeT <45585938+GRedComeT@users.noreply.github.com>
2021-04-06 16:12:28 +0800

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -4,6 +4,8 @@
 -- ------------------------------------------------------
 -- Server version	5.5.15
 
+USE tmall_springboot;
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
